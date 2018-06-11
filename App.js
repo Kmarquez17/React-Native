eb8e7e680bb374a8da866c0509df65d52d75f777
Kevin Marquez <marquezkrodriguez24@gmail.com>
2018-06-11 14:26:18 -0600

    Consumiendo API

--- App.js
+++ App.js
@@ -5,6 +5,7 @@
  */
 
 import React, { Component } from 'react';
+import Axios from "axios";
 import {
   StyleSheet,
   Text,
@@ -15,8 +16,18 @@ import ComponenteTexto from './ComponenteTexto'
 
 class ChildrenComp extends Component {
   render() {
+    if (this.props.result){
+      var res = this.props.result.map((item, i) => {
+        return(
+          <Text key={i}>{item.title}</Text>
+        )
+      })
+    }
     return (
       <View>
+        <Text>
+         {res}
+        </Text>
         <View style={this.props.estado ? styles.on : styles.off} />
       </View>
     )
@@ -26,9 +37,20 @@ class ChildrenComp extends Component {
 class App extends Component {
   constructor() {
     super()
-    this.state = { estado: false }
+    this.state = {
+       estado: false,
+       data:null
+      }
 
     this.handleClick = this.handleClick.bind(this);
+  }
+
+  componentDidMount(){
+    Axios.get('https://facebook.github.io/react-native/movies.json')
+        .then((res) => {
+          this.setState({ data: res.movies })
+          console.warn(res.data);
+        })
   }
 
   handleClick() {
@@ -40,7 +62,7 @@ class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <ChildrenComp estado={this.state.estado} />
+        <ChildrenComp estado={this.state.estado} result={this.state.data}/>
         <ComponenteTexto click={this.handleClick}/>
         <Text style={styles.welcome}>
           Welcome to React Native Kevin Marquez!
